f30779761ed6c664cdd8accfc90915c2ca27a09c
FRANCESCO BELACCA <francesco.belacca@hotmail.it>
2019-07-27 12:01:35 +0200

    Integrata la base per un game di tictactoe cambiando una parte del contesto.
    A breve commit per allineare il tutto e per avere la prima versione funzionante.

---
+++ BlazorPong/GameHub.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorPong
+{
+    public class GameHub : Hub
+    {
+        ILogger<GameHub> _logger;
+        private static readonly string BOT_GROUP = "BOT";
+
+        public GameHub(ILogger<GameHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnBotConnected()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, BOT_GROUP);
+            _logger.LogInformation("Bot joined");
+        }
+
+        public async Task OnBotDisconnected()
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, BOT_GROUP);
+            _logger.LogInformation("Bot left");
+        }
+
+        public async Task OnBotMoveReceived(string[] board, string connectionID)
+        {
+            await Clients.Client(connectionID).SendAsync("NotifyUser", board);
+        }
+
+        public async Task OnUserMoveReceived(string[] board)
+        {
+            await Clients.Group(BOT_GROUP).SendAsync("NotifyBot", board, Context.ConnectionId);
+        }
+    }
+}
